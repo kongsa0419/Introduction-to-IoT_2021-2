d2371f23626316cf7dba630593089191c85da1fb
Seungwoo Kang <swkang@koreatech.ac.kr>
2021-11-03 11:05:30 +0900

    dht11 sensor example

---
+++ sensor_DHT11/example.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as gpio
+import dht11
+import time
+import datetime
+
+# initialize GPIO
+gpio.setwarnings(True)
+gpio.setmode(gpio.BCM)
+gpio.cleanup()
+
+# read data using pin 20
+instance = dht11.DHT11(pin=20)
+
+try:
+    while True:
+        result = instance.read()
+        if result.is_valid():
+            print("Last valid input: " + str(datetime.datetime.now()))
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    gpio.cleanup()
